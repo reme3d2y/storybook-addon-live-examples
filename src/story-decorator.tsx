1b58d182bc4ba3349f6e9a0fe4958f59c1868c28
reme3d2y <AYatsenko@alfabank.ru>
2021-08-29 15:53:52 +0300

    feat: add defaultCanvas param to restore default canvas

--- src/story-decorator.tsx
+++ src/story-decorator.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { CanvasReplacer, Example } from './components';
 
 export const decorator = (storyFn: StoryFn, context: StoryContext) => {
-    if (context.viewMode !== 'docs') return storyFn();
+    if (context.viewMode !== 'docs' || context.parameters.defaultCanvas) return storyFn();
 
     const { live = true, expanded = false, storySource, scope } = context.parameters;
 
